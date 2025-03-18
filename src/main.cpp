0abc12579551062728d9c01931fa35571df7d6d2
stiangglanda® <69088823+stiangglanda@users.noreply.github.com>
2025-03-18 19:53:43 +0100

    Add button functionality for right-click action and adjust threshold values

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@
 #define VRX_PIN  32 // pin connected to VRX pin
 #define VRY_PIN  33 // pin connected to VRY pin
 #define SW_PIN   17 // ESP32 pin GPIO17 connected to SW  pin
+#define BUTTON_PIN  21
 
-#define LEFT_THRESHOLD  200
-#define RIGHT_THRESHOLD 600
-#define UP_THRESHOLD    200
+#define LEFT_THRESHOLD  150
+#define RIGHT_THRESHOLD 1000
+#define UP_THRESHOLD    150
 #define DOWN_THRESHOLD  300
 
 BleMouse bleMouse;
 int const POT_PIN = 36;  // analog pin used to connect the potentiometer
 int potVal = 0;             // variable to read the value from the analog pin
 int angle;              // variable to hold the angle for the servo motor
+bool buttonPressed = false;
 
 float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -25,6 +27,7 @@ void setup() {
   bleMouse.begin();
   pinMode(VRX_PIN, INPUT);
   pinMode(VRY_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
@@ -37,11 +40,14 @@ void loop() {
   valueX = analogRead(VRX_PIN);
   valueY = analogRead(VRY_PIN);
 
-
   Serial.print("x = ");
   Serial.print(valueX);
   Serial.print(", y = ");
-  Serial.println(valueY);
+  Serial.print(valueY);
+
+  int buttonState = digitalRead(BUTTON_PIN);
+  Serial.print(" buttonState = ");
+  Serial.println(buttonState);
 
   if(bleMouse.isConnected()) 
   {
@@ -55,5 +61,16 @@ void loop() {
       bleMouse.move(0,-1);
     else if (valueY > DOWN_THRESHOLD)
       bleMouse.move(0,1);
+    
+    if (buttonState == 0 && !buttonPressed)
+    {
+      buttonPressed = true;
+      bleMouse.click(MOUSE_RIGHT);
+    }
+
+    if (buttonState == 1 && buttonPressed)
+    {
+      buttonPressed = false;
+    }
   }
 }
